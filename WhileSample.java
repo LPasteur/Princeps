14ab2ba8ba92c65b192298de8a5718ecbf33c5d3
shinhanbit <sgksqlc@naver.com>
2018-07-23 16:55:14 +0900

    while sample

---
+++ WhileSample.java
@@ -0,0 +1,21 @@
+package ch3;
+
+import java.util.Scanner;
+
+public class WhileSample {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int count=0, n=0;
+		double sum=0;
+		System.out.println("정수를 입력하고 마지막에 0을 입력하세요");
+		while((n=scanner.nextInt())!=0) {
+			sum = sum + n;
+			count++;
+		}
+		System.out.println("수의 개수는 " + count + "개이며 ");
+		System.out.println("평균은 " + sum/count + "입니다.");
+		scanner.close();
+	}
+
+}
