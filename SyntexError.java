ea08e653b40758a887c0a03abe399a311f14d77d
shinhanbit <sgksqlc@naver.com>
2018-07-25 15:44:31 +0900

    syntex error

---
+++ SyntexError.java
@@ -0,0 +1,33 @@
+package ch3;
+
+public class SyntexError {
+
+	public static void main(String[] args) {
+/*		int x;
+		int sum = 0;
+		for(x=0; x<10; x++) {
+			sum += i;     // 컴파일(문법상) 에러
+		}
+	            ※ 예외(Exception) : 프로그래머의 부주의, 프로그램 사용상의 실수 => 
+	     예외 처리(try(예외발생코드) ~ catch(예외발생시 처리문장) ~ finally(예외발생 유무)와 관계없이 처리할 문장))
+		0으로 나누려고 할 때 : ArithmeticException
+		index가 배열 크기의 범위를 벗어날 때 : ArrayIndexOutOfBoundsException
+		객체를 생성하기 전에 사용하려고 할 때 : NullPointException
+		배열의 크기를 음수로 줄 때 : NegativeArraySizeException
+		애플릿이 보안을 위반했을 때 : SecurityException
+		
+		try {
+			예외가 예상되는 코드
+		}
+		catch(Exception유형1 e){
+			예외를 처리하는 부분
+		}
+		catch(Exception유형2 e) {
+			예외를 처리하는 부분
+		}
+		finally {
+			예외가 발생과 무관하게 처리하는 부분
+		}        */
+	}
+
+}
