f830587373cdaee236fdc169b6d444a8f891039b
shinhanbit <sgksqlc@naver.com>
2018-07-26 12:53:19 +0900

    Exercise3_3

---
+++ Exercise3_3.java
@@ -0,0 +1,13 @@
+package ch3;
+
+public class Exercise3_3 {
+
+	public static void main(String[] args) {
+		char [] c = new char[10];
+		int [] n = {0,1,2,3,4,5};
+		char [] day = {'일','월','화','수','목','금','토'};
+		double [][] d = new double[5][4];
+		int [][] val = { {1,2,3,4}, {5,6,7,8}, {9,10,11,12} };
+	}
+
+}
