a00af9223631e4b1f5f0cc1879c8e0773a3c9fdb
shinhanbit <sgksqlc@naver.com>
2018-07-31 14:40:29 +0900

    calendar print

---
+++ CalendarPrint.java
@@ -0,0 +1,20 @@
+package ch4;
+
+import java.util.Calendar;
+
+public class CalendarPrint {
+	Calendar cal = Calendar.getInstance();
+	
+	// 요일 헤더 표시
+	private String[] calHeader = {"일","월","화","수","목","금","토"};
+	
+	// 날짜 데이터 배열
+	private String[][] calDate = new String[6][7];
+	
+	// 배열의 가로길이
+	// 월 시작 요일
+	// 월 마지막 날짜 
+	// 입력 날짜
+
+	// 생성자 : 데이터 생성
+}
