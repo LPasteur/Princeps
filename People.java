fcf9d29636549ae75e9ca24ac0b8df98f79b17d5
shinhanbit <sgksqlc@naver.com>
2018-08-08 15:30:03 +0900

    people

---
+++ People.java
@@ -0,0 +1,12 @@
+package ch5;
+
+public class People {
+	public String name;
+	public String ssn;
+	
+	public People(String name, String ssn) {
+		super();
+		this.name = name;
+		this.ssn = ssn;
+	}
+}
