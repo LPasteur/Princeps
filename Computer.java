dd9a4f18b089b4d322f459a1fa4fb36e6124944d
shinhanbit <sgksqlc@naver.com>
2018-08-08 15:29:06 +0900

    computer

---
+++ Computer.java
@@ -0,0 +1,9 @@
+package ch5;
+
+public class Computer extends Calculator {
+	String name;
+	double areaCircle(double r) {
+		System.out.println("Calculator°´Ã¼ÀÇ areaCircle()½ÇÇà");
+		return Math.PI * r * r;
+	}
+}
