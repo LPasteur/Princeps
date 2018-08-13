045414305dedaa12e4efa366b49714582994a49c
shinhanbit <sgksqlc@naver.com>
2018-08-13 15:27:10 +0900

    school2 main

---
+++ School2Main.java
@@ -0,0 +1,11 @@
+package ch5;
+
+public class School2Main {
+
+	public static void main(String[] args) {
+		School2 school2 = new School2();
+		school2.eng = 100;
+		school2.kor = 90;
+		System.out.println(school2.hap());
+	}
+}
